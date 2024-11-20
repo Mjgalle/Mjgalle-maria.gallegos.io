38630c6597cccfa5a3a2013e96027c379a6e6427
Maria J Gallegos-Vallejo <mvallejo@oppgen.com>
2024-11-19 21:46:38 -0500

    adding updates to LP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
+import welcomeBack from './assets/icons/welcome-back.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={welcomeBack} className="welcome-png" alt="Welcome icons created by Freepik - Flaticon" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Personal Website :)
+          <br>
+          </br>
+          Please, <a href="">Enter</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
